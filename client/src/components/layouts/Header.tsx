9a03fa9e6647457cbff0da91f51737a955d1fd6e
HungryOcto <kke912174@gmail.com>
2023-03-10 17:58:50 +0900

    design: Button UI 추가 및 수정

--- client/src/components/layouts/Header.tsx
+++ client/src/components/layouts/Header.tsx
@@ -1,8 +1,10 @@
+import styled from 'styled-components';
 import Button from 'components/UI/Button';
+import theme from '../../theme';
 
 function Header() {
   return (
-    <>
+    <Container>
       <h1>yanoljimalja</h1>
       <ul>
         <li>자유</li>
@@ -10,11 +12,18 @@ function Header() {
         <li>질문</li>
         <li>이벤트</li>
       </ul>
-      <Button>토글</Button>
-      <Button>로그인</Button>
-      <Button>로그아웃</Button>
-    </>
+      <Button.DefaultBtn>토글</Button.DefaultBtn>
+      <Button.WhiteBtn>로그인</Button.WhiteBtn>
+      <Button.PointBtn>회원가입</Button.PointBtn>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: sticky;
+  z-index: 1;
+`;
 
 export default Header;
