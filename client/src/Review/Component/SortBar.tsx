c0704d0bed4909ae65b3c28e91ff140cd68b29ba
jiyongnoh <njy99000@naver.com>
2023-03-10 17:17:59 +0900

    feat: 정렬 바 추가

---
+++ client/src/Review/Component/SortBar.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/no-array-index-key */
+import GlobalStyle from 'GlobalStyles';
+import styled from 'styled-components';
+import { FlexContainer } from 'Review/Page/ReviewPage';
+import { useState } from 'react';
+
+function SortBar() {
+  const sortArr: string[] = ['전체', '이투스', '메가스터디', '에듀윌', '그 외'];
+  const [sortTag, setSortTag] = useState<string>('');
+
+  const sortTagHandler = (e: any) => {
+    setSortTag(e.target.value);
+  };
+
+  return (
+    <FlexContainer>
+      <GlobalStyle />
+      {sortArr.map((el, index) => {
+        return (
+          <SortButton key={index} value={el} onClick={sortTagHandler}>
+            {el}
+          </SortButton>
+        );
+      })}
+    </FlexContainer>
+  );
+}
+
+export default SortBar;
+
+const SortButton = styled.button`
+  width: 5.9rem;
+  border: none;
+  padding: 0.5rem 1rem;
+  background-color: #6667ab;
+  color: white;
+  border-radius: 5rem;
+  cursor: pointer;
+`;
