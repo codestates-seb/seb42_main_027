8aeff413feae4b0cee1e24b2d6c6b79b6a88362f
HungryOcto <kke912174@gmail.com>
2023-03-10 16:53:26 +0900

    feat: Header, Footer 컴포넌트 생성

--- client/src/components/layouts/Header.tsx
+++ client/src/components/layouts/Header.tsx
@@ -1,5 +1,20 @@
+import Button from 'components/UI/Button';
+
 function Header() {
-  return <div />;
+  return (
+    <>
+      <h1>yanoljimalja</h1>
+      <ul>
+        <li>자유</li>
+        <li>리뷰</li>
+        <li>질문</li>
+        <li>이벤트</li>
+      </ul>
+      <Button>토글</Button>
+      <Button>로그인</Button>
+      <Button>로그아웃</Button>
+    </>
+  );
 }
 
 export default Header;
---
+++ client/src/pages/FreeBoard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import styled from "styled-components";
+
+function FreeBoard() {
+  return <h1>FreeBoard Page</h1>;
+}
+
+export default FreeBoard;
