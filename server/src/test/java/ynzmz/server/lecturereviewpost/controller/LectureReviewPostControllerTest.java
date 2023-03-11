86dfcc0c4f82bda206ee1b5a3b22abc74b00eebc
Minogod <93550624+Minogod@users.noreply.github.com>
2023-03-11 23:42:59 +0900

    docs: 강의리뷰글 상세조회,삭제 docs 추가 test: 강의리뷰글 상세조회,삭제 테스트 추가

--- server/src/test/java/ynzmz/server/lecturereviewpost/controller/LectureReviewPostControllerTest.java
+++ server/src/test/java/ynzmz/server/lecturereviewpost/controller/LectureReviewPostControllerTest.java
@@ -23,10 +23,10 @@ import ynzmz.server.lecturereviewpost.sevice.LectureReviewPostService;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
@@ -195,12 +195,70 @@ class LectureReviewPostControllerTest {
     }
 
     @Test
-    void getReviewPostDetail() {
+    @DisplayName("리뷰글 상세 조회 테스트")
+    void getReviewPostDetail() throws Exception {
+        long lectureReviewPostId = 1L;
 
+        when(lectureReviewPostService.findLectureReviewPostById(anyInt())).thenReturn(new LectureReviewPost());
+        when(lectureReviewPostMapper.lectureReviewPostInfoResponseToLectureReviewPost(any())).thenReturn(lectureReviewPostInfoResponse);
+
+        ResultActions actions = mockMvc.perform(get("/lecture-review-posts/{lecture-review-post-id}",lectureReviewPostId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        );
+        actions.andExpect(status().isOk()).andDo(document("get-lecture-review-post",
+                getRequestPreProcessor(),
+                getResponsePreProcessor(),
+                pathParameters(
+                        parameterWithName("lecture-review-post-id").description("리뷰 식별번호")
+                ),
+                responseFields(
+                        List.of(
+                                fieldWithPath("data.lectureReviewPostId").type(JsonFieldType.NUMBER).description("리뷰글 식별번호"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("리뷰글 제목"),
+                                fieldWithPath("data.starPoint").type(JsonFieldType.NUMBER).description("리뷰글 평점"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("리뷰글 내용"),
+                                fieldWithPath("data.createdAt").type(JsonFieldType.STRING).description("작성시간 [서버는 string 으로 관리]"),
+                                fieldWithPath("data.modifiedAt").type(JsonFieldType.STRING).description("수정된시간 [서버는 string 으로 관리]"),
+                                fieldWithPath("data.viewCount").type(JsonFieldType.NUMBER).description("리뷰글 조회수"),
+                                fieldWithPath("data.voteCount").type(JsonFieldType.NUMBER).description("리뷰글 추천수"),
+
+                                fieldWithPath("data.lecture.lectureId").type(JsonFieldType.NUMBER).description("강의 식별번호"),
+                                fieldWithPath("data.lecture.name").type(JsonFieldType.STRING).description("강의 이름"),
+                                fieldWithPath("data.lecture.introduction").type(JsonFieldType.STRING).description("강의 소개"),
+                                fieldWithPath("data.lecture.starPointAverage").type(JsonFieldType.NUMBER).description("강의 평균평점"),
+                                fieldWithPath("data.lecture.tags").type(JsonFieldType.ARRAY).description("강의 키워드 태그"),
+                                fieldWithPath("data.lecture.teacher.teacherId").type(JsonFieldType.NUMBER).description("강의의 강사 식별번호"),
+                                fieldWithPath("data.lecture.teacher.name").type(JsonFieldType.STRING).description("강사 이름"),
+                                fieldWithPath("data.lecture.teacher.introduction").type(JsonFieldType.STRING).description("강사 소개"),
+                                fieldWithPath("data.lecture.teacher.tags").type(JsonFieldType.ARRAY).description("강사 키워드 태그"),
+
+                                fieldWithPath("data.member.memberId").type(JsonFieldType.NUMBER).description("작성자 회원 식별번호"),
+                                fieldWithPath("data.member.email").type(JsonFieldType.STRING).description("작성자 이메일"),
+                                fieldWithPath("data.member.displayName").type(JsonFieldType.STRING).description("작성자 이름"),
+                                fieldWithPath("data.member.password").type(JsonFieldType.STRING).description("작성자 비밀번호(이건 없엘예정)"),
+                                fieldWithPath("data.member.iconImageUrl").type(JsonFieldType.STRING).description("작성자 아이콘 Url"),
+                                fieldWithPath("data.member.createdAt").type(JsonFieldType.STRING).description("작성자 가입시간 [서버는 string 으로 관리]")
+                        )
+                ))
+        );
     }
 
     @Test
-    void deleteReview() {
+    @DisplayName("리뷰글 삭제 테스트")
+    void deleteReview() throws Exception {
+        long lectureReviewPostId = 1L;
 
+        ResultActions actions = mockMvc.perform(delete("/lecture-review-posts/{lecture-review-post-id}",lectureReviewPostId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        );
+        actions.andExpect(status().isOk()).andDo(document("delete-lecture-review-post",
+                getRequestPreProcessor(),
+                getResponsePreProcessor(),
+                pathParameters(
+                        parameterWithName("lecture-review-post-id").description("리뷰글 식별번호")
+                )
+        ));
     }
 }
