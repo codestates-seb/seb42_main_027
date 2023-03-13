f5388c476870f9e271865953175d7eaf101fc477
CARRO11 <eleying@naver.com>
2023-03-14 04:57:55 +0900

    feat: Event 컨트롤러 기본 완성/ 야놀지말자 이벤트에 대한 상세 필요

--- server/src/main/java/ynzmz/server/eventBoard/mapper/EventMapper.java
+++ server/src/main/java/ynzmz/server/eventBoard/mapper/EventMapper.java
@@ -4,11 +4,15 @@ import org.mapstruct.Mapper;
 import ynzmz.server.eventBoard.dto.EventDto;
 import ynzmz.server.eventBoard.entity.Event;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface EventMapper {
 
     Event eventToEventPost(EventDto.Post post);
 
     Event eventToEventResponse(EventDto.Response response);
+
+    List<Event> eventToEventResponses(List<EventDto.Response> response);
 
 }
