3393687268ca41d6de6a6c497842b3d17c169e2c
HungryOcto <kke912174@gmail.com>
2023-03-14 12:37:32 +0900

    feat: 라우터 추가

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,11 +1,17 @@
+import Main from 'pages/MainPage';
 import Login from 'pages/Login';
+import QnABoard from 'pages/QnABoard/QnABoard';
+import FreeBoard from 'pages/FreeBoard/FreeBoard';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="login" element={<Login />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/qna" element={<QnABoard />} />
+        <Route path="/free" element={<FreeBoard />} />
       </Routes>
     </BrowserRouter>
   );
