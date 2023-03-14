ed1e442bf2bc457641a65621bad8af167e43fbd8
HungryOcto <kke912174@gmail.com>
2023-03-14 13:09:14 +0900

    fix: 라우터 경로 수정

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -8,10 +8,10 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/qna" element={<QnABoard />} />
-        <Route path="/free" element={<FreeBoard />} />
+        <Route path="" element={<Main />} />
+        <Route path="login" element={<Login />} />
+        <Route path="qna" element={<QnABoard />} />
+        <Route path="free" element={<FreeBoard />} />
       </Routes>
     </BrowserRouter>
   );
