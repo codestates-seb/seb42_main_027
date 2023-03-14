427bf6a553f066d75b09ea713bd9c91f6ee4e5b2
jiyongnoh <njy99000@naver.com>
2023-03-14 21:26:22 +0900

    Feat: 리뷰 페이지 관련 라우터 추가

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,5 +1,7 @@
 import Login from 'pages/Login';
 import ReviewPage from 'pages/Review/ReviewPage';
+import ReviewPageDetail from 'pages/Review/ReviewPageDetail';
+import CreateTeacher from 'pages/Review/CreateTeacher';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function Router() {
@@ -8,6 +10,11 @@ function Router() {
       <Routes>
         <Route path="login" element={<Login />} />
         <Route path="/" element={<ReviewPage />} />
+        <Route
+          path="/ReviewPageDetail/:teacherId"
+          element={<ReviewPageDetail />}
+        />
+        <Route path="/createTeacher" element={<CreateTeacher />} />
       </Routes>
     </BrowserRouter>
   );
