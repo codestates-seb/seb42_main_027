3bd6c791e13b30c55d797a8a7143d638f3145e0e
HungryOcto <kke912174@gmail.com>
2023-03-14 11:58:52 +0900

    feat: 메인페이지, 자유게시판, 질문게시판 페이지 생성

---
+++ client/src/pages/FreeBoard/FreeBoard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function FreeBoard() {
+  return (
+    <Container>
+      <h1>FreeBoard Page</h1>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default FreeBoard;
