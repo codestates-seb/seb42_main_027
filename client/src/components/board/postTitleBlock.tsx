b87812e0a35e9fd5e8f114e9bf873c38572976e6
HungryOcto <kke912174@gmail.com>
2023-03-15 19:29:22 +0900

    feat: dummy 데이터 생성, 게시판 목록 기능 구현

--- client/src/components/board/postTitleBlock.tsx
+++ client/src/components/board/postTitleBlock.tsx
@@ -1,25 +1,90 @@
 import styled from 'styled-components';
+import theme from 'theme';
 
-function PostTitleBlock() {
+interface PostData {
+  id: number;
+  category: string;
+  username: string;
+  userimg: string;
+  title: string;
+  content: string;
+  view: string;
+  comment: string;
+  vote: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Props {
+  ele: PostData;
+}
+
+function PostTitleBlock(ele: Props) {
+  console.log('ele', ele);
+  const data = ele;
+
   return (
     <Container>
-      <div>일상</div>
-      <div>제목</div>
-      {/* <div>{post.title}</div> */}
-      <div>
-        <div>프로필이미지</div>
-        <div>닉네임</div>
-        <div>작성시간</div>
-      </div>
-      <div>
-        <div>조회수</div>
-        <div>댓글수</div>
-        <div>추천수</div>
-      </div>
+      <Category>{data.ele.category}</Category>
+      <Title>{data.ele.title}</Title>
+      <UserData>
+        <div>{data.ele.userimg}</div>
+        <div>{data.ele.username}</div>
+        <div>{data.ele.createdAt}</div>
+      </UserData>
+      <Count>
+        <div>{data.ele.view}</div>
+        <div>{data.ele.comment}</div>
+        <div>{data.ele.vote}</div>
+      </Count>
     </Container>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+  height: 100px;
+  padding: 1rem ${theme.gap.px20};
+  border-bottom: 1px solid ${theme.colors.gray};
+`;
+
+const Category = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 18px;
+  border: 1px solid ${theme.colors.pointColor};
+  border-radius: 5px;
+  font-size: ${theme.fontSizes.sm};
+  font-weight: bold;
+  color: ${theme.colors.pointColor};
+  margin-bottom: 4px;
+`;
+
+const Title = styled.div`
+  display: flex;
+  font-size: ${theme.fontSizes.md};
+  font-weight: bold;
+  margin-bottom: 6px;
+
+  &:hover {
+    color: ${theme.colors.pointColor};
+  }
+`;
+
+const UserData = styled.div`
+  display: flex;
+`;
+
+const Count = styled.div`
+  display: flex;
+  position: absolute;
+  right: ${theme.gap.px20};
+  bottom: 1rem;
+`;
 
 export default PostTitleBlock;
