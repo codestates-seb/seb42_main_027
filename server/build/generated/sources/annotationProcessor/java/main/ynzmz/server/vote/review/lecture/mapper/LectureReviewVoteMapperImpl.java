9606cefa6a28c4d40b1f03fb7521f2677178d3a0
MINHO <93550624+Minogod@users.noreply.github.com>
2023-03-16 15:29:25 +0900

    fix: 다빈님 코드 병합준비

---
+++ server/build/generated/sources/annotationProcessor/java/main/ynzmz/server/vote/review/lecture/mapper/LectureReviewVoteMapperImpl.java
@@ -0,0 +1,13 @@
+package ynzmz.server.vote.review.lecture.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-03-16T11:45:20+0900",
+    comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.1.jar, environment: Java 11.0.17 (Azul Systems, Inc.)"
+)
+@Component
+public class LectureReviewVoteMapperImpl implements LectureReviewVoteMapper {
+}
