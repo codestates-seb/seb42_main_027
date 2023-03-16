20002d3591279bbf107cc4a051a8b8e59d047bb9
codnr <wnd75913@gmail.com>
2023-03-17 02:05:56 +0900

    feat: 새로운 props 추가 및 props에 대한 스타일 동적으로 변하는 기능 추가

--- client/src/components/common/Input.tsx
+++ client/src/components/common/Input.tsx
@@ -1,39 +1,101 @@
 import styled from 'styled-components';
 import theme from 'theme';
 
-const { colors } = theme;
+const { danger, success, fontColor, pointColor, gray } = theme.colors;
+const dangerRGB = '236, 60, 60';
+const successRGB = '101, 226, 113';
 
 const Label = styled.label`
   display: flex;
   flex-direction: column;
-  color: ${colors.pointColor};
+  color: ${pointColor};
 `;
 
 const StyleInput = styled.input`
-  border: 1px solid ${colors.gray};
   padding: 0.4rem 1rem;
   border-radius: 0.5rem;
+  margin-bottom: 0.5rem;
+  background-color: ${({ color }) => {
+    switch (color) {
+      case 'danger':
+        return `rgba(${dangerRGB}, 0.2)`;
+      case 'success':
+        return `rgba(${successRGB}, 0.2)`;
+      default:
+        return 'white';
+    }
+  }};
+  border: 1px solid
+    ${({ color }) => {
+      switch (color) {
+        case 'danger':
+          return danger;
+        case 'success':
+          return success;
+        default:
+          return gray;
+      }
+    }};
+  color: ${({ color }) => {
+    switch (color) {
+      case 'danger':
+        return danger;
+      case 'success':
+        return success;
+      default:
+        return fontColor;
+    }
+  }};
+`;
+
+const StyleErrorMessage = styled.p`
   margin-bottom: 1rem;
-  color: ${colors.fontColor};
-  background-color: ${colors.danger};
-  opacity: 40%;
+  color: ${danger};
 `;
 
 type InputProps = {
-  type: string;
+  placeholder?: string;
+  errorMessage?: string;
+  color?: 'danger' | 'success';
   label: string;
+  type: string;
   id: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   value: string;
 };
 
-function Input({ label, type, id, onChange, value }: InputProps) {
+function Input({
+  placeholder,
+  errorMessage,
+  color,
+  label,
+  type,
+  id,
+  onChange,
+  value,
+}: InputProps) {
   return (
     <Label htmlFor={id}>
       {label}
-      <StyleInput onChange={onChange} type={type} id={id} value={value} />
+      <StyleInput
+        color={color}
+        placeholder={placeholder}
+        onChange={onChange}
+        type={type}
+        id={id}
+        value={value}
+      />
+      {color === 'danger' ? (
+        <StyleErrorMessage>{errorMessage}</StyleErrorMessage>
+      ) : null}
     </Label>
   );
 }
+
+Input.defaultProps = {
+  errorMessage: undefined,
+  color: undefined,
+  placeholder: undefined,
+};
 
 export default Input;
