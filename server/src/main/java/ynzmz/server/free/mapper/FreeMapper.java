1f98779f3228284918e6b512f46e51f96e05a30c
CARRO11 <eleying@naver.com>
2023-03-16 17:33:44 +0900

    feat: free 서비스 CRUD작성 + ExceptionCode 작성 //추후 멤버 작성 필요

---
+++ server/src/main/java/ynzmz/server/free/mapper/FreeMapper.java
@@ -0,0 +1,8 @@
+package ynzmz.server.free.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface FreeMapper {
+}
