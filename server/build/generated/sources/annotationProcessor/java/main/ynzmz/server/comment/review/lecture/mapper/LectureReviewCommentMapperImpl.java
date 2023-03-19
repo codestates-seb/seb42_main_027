3726a2d867a8244e14b0306feaa4822c7521b1c8
MINHO <93550624+Minogod@users.noreply.github.com>
2023-03-19 18:36:09 +0900

    fix: restdocs 설정 삭제 및 flyway 설정추가 테스트완료

--- server/build/generated/sources/annotationProcessor/java/main/ynzmz/server/comment/review/lecture/mapper/LectureReviewCommentMapperImpl.java
+++ server/build/generated/sources/annotationProcessor/java/main/ynzmz/server/comment/review/lecture/mapper/LectureReviewCommentMapperImpl.java
@@ -11,7 +11,7 @@ import ynzmz.server.member.entity.Member;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-03-19T01:16:27+0900",
+    date = "2023-03-19T16:34:36+0900",
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.1.jar, environment: Java 11.0.17 (Azul Systems, Inc.)"
 )
 @Component
