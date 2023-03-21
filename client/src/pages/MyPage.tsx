113cd31496d5c2fb380819dac141b0412281daa7
codnr <wnd75913@gmail.com>
2023-03-21 19:44:41 +0900

    feat: 내가 작성한 게시글 필터 선택에 맞는 api 요청 보내는 기능 추가

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -28,6 +28,7 @@ const ListContainer = styled.div`
 
 const PostListContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 45%;
@@ -40,20 +41,80 @@ const PostListTitleContainer = styled.div`
   width: 100%;
 `;
 
+const List = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 20rem;
+  overflow: auto;
+`;
+
+const ListItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 0;
+  border-bottom: 0.1rem solid #ccc;
+`;
+
+// 제목 컴포넌트
+const Title = styled.h2`
+  margin: 0;
+  font-size: ${fontSizes.md};
+`;
+
+// 카테고리 컴포넌트
+const Category = styled.span`
+  font-size: ${fontSizes.sm};
+  color: ${colors.fontColor};
+`;
+
+// 조회수 및 투표수 컴포넌트
+const Count = styled.span`
+  font-size: ${fontSizes.sm};
+  color: ${colors.fontColor};
+`;
+
+type PostProps = {
+  freeId: number;
+  title: string;
+  content: string;
+  category: string;
+  viewCount: number;
+  voteCount: number;
+  createdAt: string;
+  modifiedAt: null;
+  member: {
+    memberId: number;
+    displayName: string;
+    iconImageUrl: null;
+    state: string;
+  };
+  commentsListNum: number;
+};
+
 function MyPage() {
   const { userInfo } = useUserInfoStore(state => state);
+  const [freePosts, setFreePosts] = useState([]);
+  const [select, setSelect] = useState('자유 게시판');
 
-  const patchFreePosts = async () => {
+  const patchFreePosts = async (id: number, select: string) => {
     try {
-      await getFreePosts(userInfo.memberId);
+      // 테스트용 memberId 사용
+      const response = await getFreePosts(id, select);
+      setFreePosts(response);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleChangeBoard = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelect(e.target.value);
+  };
+
   useEffect(() => {
-    patchFreePosts();
-  });
+    patchFreePosts(4, select);
+  }, [select]);
 
   return (
     <Container>
@@ -62,12 +123,25 @@ function MyPage() {
         <PostListContainer>
           <PostListTitleContainer>
             <h2>내가 작성한 게시글</h2>
-            <select>
-              <option>자유 게시판</option>
-              <option>질문 게시판</option>
-              <option>강의 리뷰</option>
+            <select onChange={handleChangeBoard} value={select}>
+              <option value="자유 게시판">자유 게시판</option>
+              <option value="질문 게시판">질문 게시판</option>
+              <option value="강의 리뷰">강의 리뷰</option>
             </select>
           </PostListTitleContainer>
+          <List>
+            {freePosts.map((post: PostProps) => {
+              return (
+                <ListItem key={post.freeId}>
+                  <Category>{post.category}</Category>
+                  <Title>{post.title}</Title>
+                  <Count>
+                    조회수: {post.viewCount} | 투표수: {post.voteCount}
+                  </Count>
+                </ListItem>
+              );
+            })}
+          </List>
         </PostListContainer>
 
         <PostListContainer>
