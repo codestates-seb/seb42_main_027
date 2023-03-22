052cc7b5d5c3f7d4bb544594cb4e65e5d3de8b7e
CARRO11 <eleying@naver.com>
2023-03-23 07:22:25 +0900

    feat: 자체이벤트 게시판 기능 추가

--- server/src/main/java/ynzmz/server/board/event/their/service/EventService.java
+++ server/src/main/java/ynzmz/server/board/event/their/service/EventService.java
@@ -1,5 +1,7 @@
 package ynzmz.server.board.event.their.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -13,10 +15,12 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class EventService {
 
     //Create와 Delete 만 구현 --> 더 필요할까? --> 야놀지말자 고유 이벤트도 포함시켜야 함
-EventRepository eventRepository;
+private final EventRepository eventRepository;
 
 //--------------------------------------------CREATE--------------------------------------------------------
     public Event createEvent(Event event){
@@ -39,14 +43,7 @@ EventRepository eventRepository;
         return eventRepository.findAll(PageRequest.of(page,size, Sort.by("eventId")));
     }
 //-------------------------------------------UPDATE---------------------------------------------------------
-//    public void updateEvent(Event event){//추후 고유 이벤트 내용이 확정되면 이야기 하는 것으로
-//        Optional<String> yzevent = Optional.of(event.getSource().getSourceType()).orElseThrow(()-> new BusinessLogicException())
-//       Event foundEvent = findEvent(event.getEventId());
-//
-//       Optional.ofNullable(event.getDate()).ifPresent(foundEvent::setDate);
-//       Optional.ofNullable(event.getTitle()).
-//
-//    }
+
 //
 
 
