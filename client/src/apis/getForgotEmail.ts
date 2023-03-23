7c4868fa3183ac223206d9334660d99938af48e4
codnr <wnd75913@gmail.com>
2023-03-23 16:12:07 +0900

    feat: email 찾기기능 구현

---
+++ client/src/apis/getForgotEmail.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+type SignUpParams = {
+  username: string;
+  phoneNumber: string;
+};
+
+const getForgotEmail = async (pathData: SignUpParams) => {
+  await axios.post(`${apiUrl}/members`, pathData, {
+    headers: {
+      'ngrok-skip-browser-warning': '69420',
+    },
+  });
+};
+
+export default getForgotEmail;
