ec9f8d385079d2b1c9f95a7e9820024a0e126661
HungryOcto <kke912174@gmail.com>
2023-03-23 15:27:16 +0900

    fix: Container div CSS 코드 수정

--- client/src/pages/QnABoard.tsx
+++ client/src/pages/QnABoard.tsx
@@ -30,7 +30,6 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100%;
 `;
 
 const Main = styled.div`
@@ -39,7 +38,6 @@ const Main = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100%;
   max-width: 62.5%;
   margin: 0 auto;
 `;
