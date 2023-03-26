2814306bdbcb8ddc25354ab383e1604c3f0e79d8
jiyongnoh <njy99000@naver.com>
2023-03-26 17:43:57 +0900

    Design: 강의 리뷰 리스트 디자인 변경

--- client/src/components/review/LectureIntro.tsx
+++ client/src/components/review/LectureIntro.tsx
@@ -3,6 +3,9 @@
 import GlobalStyle from 'GlobalStyles';
 import styled from 'styled-components';
 import { FlexContainer } from 'pages/review/TeacherList/ReviewPage';
+import useUserInfoStore from 'stores/userInfoStore';
+import { Link } from 'react-router-dom';
+import Button from 'components/common/Button';
 
 type Props = {
   lecture: {
@@ -18,17 +21,42 @@ type Props = {
 };
 
 function LectureIntro({ lecture, teacher }: Props) {
+  const { userInfo } = useUserInfoStore(state => state);
+  const Authorization = localStorage.getItem('token');
   return (
-    <FlexContainer dir="col" width="90%" gap="1rem">
-      <BigFont>{teacher.name}</BigFont>
-      <BigFont>{lecture.title}</BigFont>
+    <FlexContainer
+      width="100%"
+      gap="1rem"
+      padding="1rem 1rem"
+      borderRadius="1rem"
+      justify="space-between"
+    >
+      <BigFont>
+        <MidGrayFont>{teacher.name}</MidGrayFont>
+        {lecture.title}
+      </BigFont>
+      <MidGrayFont />
+      <FlexContainer display={Authorization ? 'flex' : 'none'}>
+        <Link to="create">
+          <PButton>리뷰 등록</PButton>
+        </Link>
+      </FlexContainer>
     </FlexContainer>
   );
 }
 
 export default LectureIntro;
 
+const PButton = Button.PointBtn;
+
 const BigFont = styled.div`
   font-size: large;
   font-weight: bold;
+  color: black;
+`;
+
+const MidGrayFont = styled.div`
+  font-weight: 500;
+  color: gray;
+  margin-bottom: 0.5rem;
 `;
