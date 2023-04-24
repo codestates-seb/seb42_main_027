1916b95212bcbf465cbddff2d5225f7c94f2cbe5
jiyongnoh <njy99000@naver.com>
2023-04-24 17:56:45 +0900

    feat: recoil 라이브러리 추가 및 스토어 설정

---
+++ client/src/stores/bookCartStore.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
+import { atom, selectorFamily, selector } from 'recoil';
+import { BookListDataType } from 'pages/review/Book/BookLists';
+
+export type cartListType = BookListDataType & {
+  count: number;
+};
+
+export const cartList = atom<cartListType[]>({
+  key: 'cartList',
+  default: [],
+});
+
+// 현재 cartList의 중복 여부 검사
+export const includeCartList = selectorFamily({
+  key: 'IncludeCartList',
+  get:
+    id =>
+    ({ get }) => {
+      const cart = get(cartList);
+      if (!cart.length) return false;
+      return cart.filter(el => el.id === id).length;
+    },
+});
+
+export const allPrice = selector({
+  key: 'allPrice',
+  get: ({ get }) => {
+    const cart = get(cartList);
+    if (!cart.length) return 0;
+
+    return cart.reduce((acc, cur) => {
+      return (acc += cur.count * cur.price);
+    }, 0);
+  },
+});
