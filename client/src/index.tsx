685ef52e1d10a7d3ab0480ddd4eb46e0d50c2f9b
jiyongnoh <njy99000@naver.com>
2023-04-24 17:57:22 +0900

    feat: index에 recoil root 감싸기

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+// 리코일 사용을 위해 루트를 감싸줌
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-root.render(<App />);
+root.render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>,
+);
